011a86fce1fc11f0ef051ec589042bdd9a7e2342
unknown <realhandcuffs@gmail.com>
2020-05-16 14:36:30 +0200

    Refine find_tools.sh script, can now output variables in format KEY="VALUE".

--- scripts/find_tools.sh
+++ scripts/find_tools.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# this script looks for the required tools and sets env variables that point to them:
+# this script looks for the required tools:
 # - TOOL_7ZIP points to 7z.exe
 # - DIR_FALLOUT4 points to the Fallout 4 directory
 # - DIR_FALLOUT4CREATIONKIT points to the Fallout 4 Creation Kit directory (usually the same as DIR_FALLOUT4)
+# if the -g argument is passed, it will output the above variables in format KEY="VALUE"
+# if not, it will just check for presence of the tools and print human-readable messages
 
 # make sure we clean up on exit
 ORIGINAL_DIR=$(pwd)
@@ -13,16 +15,21 @@ function clean_up {
 trap clean_up EXIT
 
 # check arguments
-QUIET=0
+GENERATE=0
 for VAR in "$@"
 do
   case "$VAR" in
-    "-q" )
-      QUIET=1;;
-    "-h" )
-      echo "Usage: $(basename "$0") [-q]" && exit -1;;
+    "-g" )
+      GENERATE=1;;
+    "--generate" )
+      GENERATE=1;;
     * )
-      echo "Invalid argument: $VAR" && exit -1;;
+      if [[ "$VAR" != "-h" && "$VAR" != "--help" ]]
+      then
+        echo "Invalid argument: $VAR"
+      fi
+      echo "Usage: $(basename "$0") [-g]"
+      exit -1;;
   esac
 done
 
@@ -34,26 +41,26 @@ cd "$BASE_DIR"
 # search for 7zip
 # if 7z.exe (or symlink to it) is in tools directory, this is used
 # otherwise try to find the install location in the registry
-if [ -f "tools/7z.exe" ]
+if [[ -f "tools/7z.exe" ]]
 then
-  [[ $QUIET == 0 ]] && echo "7-Zip: In tools directory."
+  [[ $GENERATE == 0 ]] && echo "7-Zip: In tools directory."
   TOOL_7ZIP="$(realpath "$BASE_DIR/tools/7z.exe")"
-  if [ "$TOOL_7ZIP" != "$BASE_DIR/tools/7z.exe" ]
+  if [[ "$TOOL_7ZIP" != "$BASE_DIR/tools/7z.exe" ]]
   then
-    [[ $QUIET == 0 ]] && echo "  Resolved to: $TOOL_7ZIP"
+    [[ $GENERATE == 0 ]] && echo "  Resolved to: $TOOL_7ZIP"
   fi
 else
   REGISTRY=$(reg query "HKLM\SOFTWARE\7-Zip") || { >&2 echo "ERROR: Unable to find 7zip registry key."; exit 1; }
   PATH_7ZIP=$(echo "$REGISTRY" | sed -rn "s/\s*Path64\s+REG_SZ\s+(.*)/\1/p" | sed 's/\\/\//g' | sed 's/://')
   PATH_7ZIP="/${PATH_7ZIP%/}"
-  if [ -f "$PATH_7ZIP/7z.exe" ]
+  if [[ -f "$PATH_7ZIP/7z.exe" ]]
   then
     TOOL_7ZIP="$PATH_7ZIP/7z.exe"
-    [[ $QUIET == 0 ]] && echo "7-Zip: $TOOL_7ZIP"
+    [[ $GENERATE == 0 ]] && echo "7-Zip: $TOOL_7ZIP"
   fi
 fi
 
-if [ -z "$TOOL_7ZIP" ]
+if [[ -z "$TOOL_7ZIP" ]]
 then
   >&2 echo "ERROR: Unable to find 7-Zip."
   exit 1
@@ -62,26 +69,26 @@ fi
 # search for Fallout 4
 # if "Fallout 4" folder is in tools directory (probably symlink), this is used
 # otherwise try to find the install location in the registry
-if [ -d "tools/Fallout 4" ]
+if [[ -d "tools/Fallout 4" ]]
 then
-  [[ $QUIET == 0 ]] && echo "Fallout 4: In tools directory."
+  [[ $GENERATE == 0 ]] && echo "Fallout 4: In tools directory."
   DIR_FALLOUT4="$(realpath "$BASE_DIR/tools/Fallout 4")"
-  if [ "$DIR_FALLOUT4" != "$BASE_DIR/tools/Fallout 4" ]
+  if [[ "$DIR_FALLOUT4" != "$BASE_DIR/tools/Fallout 4" ]]
   then
-    [[ $QUIET == 0 ]] && echo "  Resolved to: $DIR_FALLOUT4"
+    [[ $GENERATE == 0 ]] && echo "  Resolved to: $DIR_FALLOUT4"
   fi
 else
   REGISTRY=$(reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 377160") || { >&2 echo "ERROR: Unable to find Fallout 4 registry key."; exit 2; }
   PATH_FALLOUT4=$(echo "$REGISTRY" | sed -rn "s/\s*InstallLocation\s+REG_SZ\s+(.*)/\1/p" | sed 's/\\/\//g' | sed 's/://')
   PATH_FALLOUT4="/${PATH_FALLOUT4%/}"
-  if [ -d "$PATH_FALLOUT4" ]
+  if [[ -d "$PATH_FALLOUT4" ]]
   then
     DIR_FALLOUT4="$PATH_FALLOUT4"
-    [[ $QUIET == 0 ]] && echo "Fallout 4: $DIR_FALLOUT4"
+    [[ $GENERATE == 0 ]] && echo "Fallout 4: $DIR_FALLOUT4"
   fi
 fi
 
-if [ -z "$DIR_FALLOUT4" ]
+if [[ -z "$DIR_FALLOUT4" ]]
 then
   >&2 echo "ERROR: Unable to find Fallout 4."
   exit 2
@@ -90,26 +97,32 @@ fi
 # search for Fallout 4 Creation Kit
 # if "Fallout 4 Creation Kit" folder is in tools directory (probably symlink), this is used
 # otherwise try to find it in the same folder as Fallout 4
-if [ -d "tools/Fallout 4 Creation Kit" ]
+if [[ -d "tools/Fallout 4 Creation Kit" ]]
 then
-  [[ $QUIET == 0 ]] && echo "Fallout 4 Creation Kit: In tools directory."
+  [[ $GENERATE == 0 ]] && echo "Fallout 4 Creation Kit: In tools directory."
   DIR_FALLOUT4CREATIONKIT="$(realpath "$BASE_DIR/tools/Fallout 4 Creation Kit")"
-  if [ "$DIR_FALLOUT4CREATIONKIT" != "$BASE_DIR/tools/Fallout 4 Creation Kit" ]
+  if [[ "$DIR_FALLOUT4CREATIONKIT" != "$BASE_DIR/tools/Fallout 4 Creation Kit" ]]
   then
-    [[ $QUIET == 0 ]] && echo "  Resolved to: $DIR_FALLOUT4CREATIONKIT"
+    [[ $GENERATE == 0 ]] && echo "  Resolved to: $DIR_FALLOUT4CREATIONKIT"
   fi
 else
-  if [ -f "$DIR_FALLOUT4/CreationKit.exe" ]
+  if [[ -f "$DIR_FALLOUT4/CreationKit.exe" ]]
   then
     DIR_FALLOUT4CREATIONKIT="$DIR_FALLOUT4"
-    [[ $QUIET == 0 ]] && echo "Fallout 4 Creation Kit: In Fallout 4 directory."
+    [[ $GENERATE == 0 ]] && echo "Fallout 4 Creation Kit: In Fallout 4 directory."
   fi
 fi
 
-if [ -z "$DIR_FALLOUT4CREATIONKIT" ]
+if [[ -z "$DIR_FALLOUT4CREATIONKIT" ]]
 then
   >&2 echo "ERROR: Unable to find Fallout 4 Creation Kit."
   exit 3
 fi
 
-# done, success!
+# done, echo commands to set environment variables if requested to do so 
+if [[ $GENERATE == 1 ]]
+then
+    echo TOOL_7ZIP=\"$TOOL_7ZIP\"
+    echo DIR_FALLOUT4=\"$DIR_FALLOUT4\"
+    echo DIR_FALLOUT4CREATIONKIT=\"$DIR_FALLOUT4CREATIONKIT\"
+fi
